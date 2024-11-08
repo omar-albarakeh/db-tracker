6decb7aecd8ee42d0e00cdc80b84f4822cc2568e
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-09 01:20:00 +0200

    php <> register verification added

---
+++ api/register.php
@@ -0,0 +1,23 @@
+<?php
+include "../api/cdb.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $stmt = $connection->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $username, $password);
+
+    if ($stmt->execute()) {
+        if ($stmt->affected_rows > 0) {
+            echo 'Registration successful! <a href="../html/login.html">Login here</a>';
+        } else {
+            echo 'Error: No rows were inserted. Please try again.';
+        }
+    } else {
+        echo 'Error executing query. Please try again.';
+    }
+
+    $stmt->close();
+}
+?>
